a87103620b9df2454c9a096a4c6fa91d152c12bf
Choiyeojin <choiyj0303@naver.com>
2022-03-22 22:20:56 +0900

    commit111

---
+++ src/Date3_22_1.java
@@ -0,0 +1,10 @@
+public class Date3_22_1 {
+    public static void main(String[] args) {
+        //p.95
+        System.out.printf("\n줄바꿈\n연습 \n");
+        System.out.printf("\t탭키\t연습 \n");
+        System.out.printf("이것을\r덮어씁니다 \n");
+        System.out.printf("글자가 \"강조\"되는 효과 \n");
+        System.out.printf("\\\\\\ 역슬래시 세개 출력 \n");
+    }
+}
