c51aaa27cc841ae10dbb7efa86e97dc5d1682101
Choiyeojin <choiyj0303@naver.com>
2022-03-26 00:06:42 +0900

    5

---
+++ src/Date3_26_1.java
@@ -0,0 +1,11 @@
+public class Date3_26_1 {
+    public static void main(String[] args) {
+        int thr = 3;
+        int fou = 4;
+        int num = 0;
+
+        System.out.println(thr + "줄");
+        System.out.println(thr + " X " + ++num + " = " + thr * num);
+    }
+
+}
