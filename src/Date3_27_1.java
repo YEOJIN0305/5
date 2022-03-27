c0cd0d78a0e68d2ca6187978f900d57fb1fa2fa4
Choiyeojin <choiyj0303@naver.com>
2022-03-27 23:25:19 +0900

    5

---
+++ src/Date3_27_1.java
@@ -0,0 +1,9 @@
+public class Date3_27_1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        System.out.println(a + b);
+        System.out.printf("a + b = %d", a+b );
+    }
+}
